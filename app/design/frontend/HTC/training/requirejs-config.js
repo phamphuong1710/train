00f4d9739529c13ed742d8a70b491c187d3a0c9f
Phương Phạm <phuongpham.96op@gmail.com>
2020-04-13 15:09:42 +0700

    create header

---
+++ app/design/frontend/HTC/training/requirejs-config.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+var config = {
+    map: {
+        '*': {
+            'ajaxQty': 'Vendor_Module/js/jquery.cartAjaxQty',
+            'catalogAddToCart': 'Magento_Catalog/js/catalog-add-to-cart',
+        }
+    }
+};
